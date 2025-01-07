8c594013bb4e5932c4af38abbca4a3f7f5099728
SrikarMSIS <srikar.msismpl2024@learner.manipal.edu>
2025-01-07 14:24:30 +0530

    Further Steps in Syntheis

--- synthesis_main_1_1.py
+++ synthesis_main_1_1.py
@@ -7,7 +7,6 @@ import os
 import glob
 
 # Configure logging
-
 logging.basicConfig(
 
     level=logging.INFO,  # Set the logging level to INFO
@@ -20,18 +19,25 @@ logging.basicConfig(
 
 class Synthesis:
     #Constructor
-    def __init__(self, workingPath, rtlFile, constraints, tech):
-        self.workingPath = workingPath
+    def __init__(self, tech, rtlFile, effortSet, constraints, format):
         self.logPath = None
         self.rtlFile = rtlFile
         self.constraints = constraints
         self.technology = tech
+        self.effort = effortSet
+        self.format = format
     
     def adminJob(self):
+        """
+        This function is used to initially run the logging info of the job
+
+        Inputs: Self
+        Returns: None
+
+        """
         try:
-            logging.info("----WELCOME TO JENKINS AUTOMATION----")
-            logging.info("----RUNNING SYNTEHSIS----")
-            logging.info(f"Working Path: {self.workingPath}")
+            logging.info("----WELCOME TO JENKINS AUTOMATION")
+            logging.info("----RUNNING SYNTEHSIS")
             logging.info(f"Technology : {self.technology}")
             logging.info(f"RTL File : {self.rtlFile}")
         except Exception as exception:
@@ -40,16 +46,35 @@ class Synthesis:
             sys.exit()
         return 0
 
+    def chngDir(self):
+        """
+        Change the Directory and Copy the RTL File from the previous location
+        to the new self.logPath location
+
+        Inputs: Self
+        Outputs: None
+
+        """
+        try:
+            logging.info("----Changing the working path")
+        except Exception as exception:
+            logging.error(f"Exception: {exception}")
+            logging.error(f"Exiting Execution")
+        return 0
+
+
 def main():
     try:
         if len(sys.argv) > 1:
-            workingPath = sys.argv[1] 
+            tech = sys.argv[1]
             rtlFile = sys.argv[2]
-            tech = sys.argv[3]
-            constraints = sys.argv[1]
+            effortSet = sys.argv[3]
+            constraints = sys.argv[4]
+            format = sys.argv[5]
             #Create instance
-            syn = Synthesis(workingPath, rtlFile, constraints, tech)
+            syn = Synthesis(tech, rtlFile, effortSet, constraints, format)
             syn.adminJob()
+            syn.chngDir()
         else:
             logging.info("No Parameter Passed")
     except Exception as exception:
