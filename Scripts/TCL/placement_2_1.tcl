7c8cc58765601b881f5adff09f1dfdf8c1ed2d6c
SrikarMSIS <srikar.msismpl2024@learner.manipal.edu>
2025-05-21 18:16:59 +0530

    placement tcl change - io pins

--- Scripts/TCL/placement_2_1.tcl
+++ Scripts/TCL/placement_2_1.tcl
@@ -5,10 +5,11 @@ read_db setupComplete.inn
 set_db design_process_node {tech}
 #On Chip Variation of Deciding Metal Layers - Delay Caluclation
 set_db timing_analysis_type ocv
+set_db place_global_place_io_pin true
 #This is used to look into the best case for BOTH setup and hold conditions along the clock path
 set_db timing_analysis_cppr both
 
-place_opt_design -place_global_place_io_pins TRUE
+place_opt_design
 
 write_db placeCompleted.inn
 
