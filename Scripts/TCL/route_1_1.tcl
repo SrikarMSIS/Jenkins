57a58c1efdc8bb8ecd66e1f20d319659edbe8e14
SrikarMSIS <srikar.msismpl2024@learner.manipal.edu>
2025-05-21 23:28:39 +0530

    check route tcl

--- Scripts/TCL/route_1_1.tcl
+++ Scripts/TCL/route_1_1.tcl
@@ -4,9 +4,19 @@ set_db design_process_node 45
 set_db timing_analysis_type ocv 
 set_db timing_analysis_cppr both
 
-create_route_type -name srikRoute
+gui_show
 
-route_design
+set_db route_design_with_timing_driven 1
+set_db route_design_with_si_driven 1
+set_db route_design_top_routing_layer 11
+set_db route_design_bottom_routing_layer 1
+set_db route_design_detail_end_iteration 1
+set_db route_design_with_timing_driven true
+set_db route_design_with_si_driven true
+route_design -global_detail
+set_db route_special_via_connect_to_shape { noshape }
+route_special -connect core_pin -layer_change_range { Metal1(1) Metal11(11) } -block_pin_target nearest_target -core_pin_target first_after_row_end -allow_jogging 1 -crossover_via_layer_range { Metal1(1) Metal11(11) } -nets { VDD VSS } -allow_layer_change 1 -target_via_layer_range { Metal1(1) Metal11(11) }
+
 write_db route.inn
 set_db extract_rc_engine post_route
 set_db extract_rc_effort_level medium
@@ -14,5 +24,3 @@ time_design -post_route
 time_design -post_route -hold
 opt_design -post_route -setup -hold
 write_db postroute.inn
-
-gui_show
